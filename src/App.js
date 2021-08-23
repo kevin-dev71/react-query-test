d4d01d7345be19820d922d7c12f31d700d31cc7b
Kevin Dev <kevin.developer71@gmail.com>
2021-08-22 22:00:16 -0600

    react-query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from "react-query"
+import "./App.css"
+
+const fetchPokemonData = async (pokemon) => {
+  let url = pokemon.url // <--- https://pokeapi.co/api/v2/pokemon/1/
+  const pokemonData = await fetch(url)
+  const result = await pokemonData.json()
+  return result
+}
+
+const fetchPokemons = async () => {
+  const allPokemon = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=20&offset=40"
+  )
+  const pokemonsList = await allPokemon.json()
+  const pokemonsListWithData = []
+
+  for (const pokemon of pokemonsList.results) {
+    const pokemonWithData = await fetchPokemonData(pokemon)
+    pokemonsListWithData.push(pokemonWithData)
+  }
+
+  return pokemonsListWithData
+}
+
+export const usePokemons = () => useQuery(["pokemons"], fetchPokemons)
+
+function ComponentOne() {
+  const { data } = usePokemons()
+
+  return (
+    <div className="flex-column">
+      <h2>Component One</h2>
+      {data?.map((pokemon) => (
+        <div key={pokemon.id}>
+          <span>Name: </span> <span>{pokemon.name}</span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function ComponentTwo() {
+  // ✅ will get exactly the same data as ComponentOne
+  const { data } = usePokemons()
+
+  return (
+    <div className="flex-column">
+      <h2>Component Two</h2>
+      {data?.map((pokemon) => (
+        <div key={pokemon.id}>
+          <span>Name: </span> <span>{pokemon.name}</span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QueryClientProvider client={queryClient}>
+      <div className="flex">
+        <ComponentOne />
+        <ComponentTwo />
+      </div>
+    </QueryClientProvider>
+  )
 }
 
-export default App;
+export default App
